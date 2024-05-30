15a408127bfbefd7192cb5be9b624eff10ddf14d
DomiEncore <dominik.klotz@outlook.com>
2024-05-30 22:59:33 +0200

    minoradaptions to bisectionSearchRoot

--- CodeExamples/bisectionSearchRoot.py
+++ CodeExamples/bisectionSearchRoot.py
@@ -1,8 +1,11 @@
+import math
 
-epsilon = float(input("Set Epsilon: "))
+ljustint=40
+
+epsilon = float(input("Set Epsilon: ".ljust(ljustint+1)))
 
 
-cube = float(input("Finde cube root of which number: "))
+cube = float(input("Finde cube root of which number: ".ljust(ljustint+1)))
 while cube != 0:
     if cube == "":
         exit
@@ -11,14 +14,17 @@ while cube != 0:
     high = cube
     guess = (high + low) / 2.0
 
-    while abs(guess**3 - cube ) >= epsilon:
+    while abs(guess**3 - cube ) >= epsilon and num_guesses<50000:
         if guess**3 < cube:
             low = guess
         else:
             high = guess
         guess = (high + low) / 2.00
         num_guesses += 1
-    print(round(guess,3), '  (', num_guesses,')')
+
+    print("Number of guesses:".ljust(ljustint), num_guesses)
+    print("Cuberoot by bisectionSearch:".ljust(ljustint), round(guess,3))
+    print("Exact cuberoot:".ljust(ljustint),math.cbrt(cube))
     print("")
     
-    cube = float(input("Finde cube root of which number: "))
+    cube = float(input("Finde cube root of which number: ".ljust(ljustint+1)))
